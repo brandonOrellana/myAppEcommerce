0f07c10b4735f30c8cd039a132f84c5047fc8664
brandonOrellana <brandon.rojas.orellana@gmail.com>
2022-08-03 21:09:00 -0300

    feat:constants

--- src/app/modules/user/user-detail/user-detail.component.ts
+++ src/app/modules/user/user-detail/user-detail.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { USERS_DATA } from '@data/constants/users.const';
+import { ICardUser } from '@shared/components/cards/card-user/icard-user.metadata';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.scss']
 })
-export class UserDetailComponent implements OnInit {
-
+export class UserDetailComponent{
+  public users: ICardUser[] = USERS_DATA;
   public id: number;
+  public currentUser?: ICardUser;
   constructor(
     private route: ActivatedRoute,
   ) { 
     this.id = this.route.snapshot.params['id'];
+    this.currentUser = this.users.find(user => user.id === +this.id);
   }
-
-  ngOnInit(): void {
-  }
-
 }
